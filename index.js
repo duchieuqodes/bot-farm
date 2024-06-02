bcd900901f3d9981ace6cc5343880c84cf1a32eb
duchieu1999 <46242272+duchieu1999@users.noreply.github.com>
2024-06-03 00:14:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1787,9 +1787,9 @@ const cloudinary = {
   api_secret: '02v-rlQstSdcpd_6IekFwQ-tdNA'
 };
 
-// Hàm để loại bỏ emoji và ký tự đặc biệt từ fullname
+// Hàm để loại bỏ emoji từ fullname, giữ lại các ký tự tiếng Việt có dấu
 function sanitizeFullname(fullname) {
-  return fullname.replace(/[^a-zA-Z\s]/g, '').trim();
+  return fullname.replace(/[\u{1F600}-\u{1F64F}|\u{1F300}-\u{1F5FF}|\u{1F680}-\u{1F6FF}|\u{1F700}-\u{1F77F}|\u{1F780}-\u{1F7FF}|\u{1F800}-\u{1F8FF}|\u{1F900}-\u{1F9FF}|\u{1FA00}-\u{1FA6F}|\u{1FA70}-\u{1FAFF}|\u{2600}-\u{26FF}|\u{2700}-\u{27BF}]/gu, '').trim();
 }
 
 // Hàm để tạo URL ảnh với văn bản tùy chỉnh
